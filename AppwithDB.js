d74ccae7e15f3de658aab260ba5c2a76f3f2fdd1
tomcloudhm <tom@cloudhm.co.th>
2021-11-18 15:46:32 +0700

    App with DB 2nd

--- AppwithDB.js
+++ AppwithDB.js
@@ -35,56 +35,47 @@ let databaseName = "user-account";
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
-   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-     if (err) throw err;
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
 
-     let db = client.db(databaseName);
-     userObj['userid'] = 1;
+    let db = client.db(databaseName);
+    userObj['userid'] = 1;
 
-     let myquery = { userid: 1 };
-     let newvalues = { $set: userObj };
+    let myquery = { userid: 1 };
+    let newvalues = { $set: userObj };
 
-     db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-       if (err) throw err;
-       client.close();
-     });
+    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
+    });
 
-  // });
-  //userObj = {
-  //  'name': 'wisury',
-  //  'email': 'wisury@gmail.com',
-  //  'interests': 'kpop'
-  //};
+  });
   // Send response
   res.send(userObj);
 });
 
 app.get('/get-profile', function (req, res) {
-  let response = {
-    'name': '',
-    'email': '',
-    'interests': ''
-  };
-  // // Connect to the db
-  // MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-  //   if (err) throw err;
+  let response = {};
+  // Connect to the db
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
 
-  //   let db = client.db(databaseName);
+    let db = client.db(databaseName);
 
-  //   let myquery = { userid: 1 };
+    let myquery = { userid: 1 };
 
-  //   db.collection("users").findOne(myquery, function (err, result) {
-  //     if (err) throw err;
-  //     response = result;
-  //     client.close();
+    db.collection("users").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      response = result;
+      client.close();
 
-  //     // Send response
-  //     res.send(response ? response : {});
-  //   });
-  // });
-  res.send(response);
+      // Send response
+      res.send(response ? response : {});
+    });
+  });
 });
 
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
+
